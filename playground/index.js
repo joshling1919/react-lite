8003341bdb14c49d505a2b55ea748e4d9b98c14e
Joshua Ling <joshling1919@gmail.com>
2020-04-10 09:03:42 -0700

    Separate rendering code out into playground dir

---
+++ playground/index.js
@@ -0,0 +1,11 @@
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+const container = document.getElementById("root");
+const element = Didact.createElement();
+Didact.render(element, container);
